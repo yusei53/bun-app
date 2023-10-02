f64215593127c6b850cc2a534fa93169b2f1dbf2
yusei53 <yusei530922@gmail.com>
2023-10-02 10:02:27 +0900

    Bun.serveを使用して簡単なHTTPサーバーを実装する

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3001,
+  fetch(req) {
+    return new Response(`Bun!`);
+  },
+});
+console.log(`Listening on http://localhost:${server.port} ...`);
